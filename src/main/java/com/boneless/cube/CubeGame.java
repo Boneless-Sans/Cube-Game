d5a4ee01a30211bd14ff7b4b647ba5c8e3fadda0
Boneless-Sans <bonelessmail0101@gmail.com>
2024-02-08 10:18:43 -0800

    Working on menu

--- src/main/java/com/boneless/cube/CubeGame.java
+++ src/main/java/com/boneless/cube/CubeGame.java
@@ -2,6 +2,8 @@ package com.boneless.cube;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Arrays;
@@ -22,12 +24,14 @@ public class CubeGame extends JFrame implements KeyListener {
     private int height;
     private static JFrame frame = null;
     private JPanel[][] gameBoard;
+    private JPanel menuPanel;
     private Player player;
     public CubeGame(){
         frame = this;
-        parseBoardSize(board1);
-        initUI();
+        initMenu();
         addKeyListener(this);
+
+        setVisible(true);
     }
     private void parseBoardSize(int[][] map){
         boardWidth = map.length;
@@ -39,21 +43,47 @@ public class CubeGame extends JFrame implements KeyListener {
         height = 100 * boardHeight;
     }
     private void initMenu(){
-
-    }
-    private void initUI(){
         try{
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
-        setSize(width, height);
+        setSize(500,500);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setTitle("I really need a name");
-        //setIconImage(new ImageIcon("src/main/resources/assets/textures/player.png").getImage());
 
+        menuPanel = new JPanel();
+        menuPanel.add(createMenuButton("Levels", 0));
+        menuPanel.add(createMenuButton("Map Maker", 1));
+        menuPanel.add(createMenuButton("Exit", 2));
+
+    }
+    private JButton createMenuButton(String text, int buttonCode){
+        JButton button = new JButton(text);
+        button.setFocusable(false);
+        button.setFont(new Font("Arial", Font.PLAIN, 15));
+        button.addActionListener(menuButtonListener(buttonCode));
+        return button;
+    }
+    private ActionListener menuButtonListener(int buttonCode){
+        return e -> {
+            switch (buttonCode){
+                case 1 -> {
+                    menuPanel.removeAll();
+                    menuPanel.add(createMenuButton("uh",5));
+                }//unload current buttons and add buttons per pre-made level
+                case 2 -> {
+                    dispose();
+                    new CubeGame();
+                }//load map maker
+                case 3 -> System.exit(0);
+                default -> System.err.println("Invalid Button code. Button Code: " + buttonCode);
+            }
+        };
+    }
+    private void initUI(){
         JPanel masterBoard = new JPanel(new GridLayout(5,5,1,1));
         masterBoard.setBackground(Color.gray);
 
@@ -76,7 +106,6 @@ public class CubeGame extends JFrame implements KeyListener {
         player = new Player(newX,newY);
 
         gameLoop();
-        setVisible(true);
     }
     private void gameLoop(){
         Thread checkLoop = new Thread(() -> {
@@ -138,9 +167,9 @@ public class CubeGame extends JFrame implements KeyListener {
             }
 
             if(newX >= 0 && newX < boardWidth && newY >= 0 && newY < boardHeight){
-                //check walld
+                //check wall collision
                 if(boardData[newX][newY] != 1) {
-                    //check entity
+                    //check entity collision
                     if(boardData[newX][newY] == 3){
                         boardData[x][y] = 0;
                         try{
